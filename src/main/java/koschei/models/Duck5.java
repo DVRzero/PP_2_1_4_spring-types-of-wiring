d58f2a7c5788d6d9f45eedb0d1d8474bf976e355
DVR <age.star@mail.ru>
2023-01-28 20:55:32 +0300

    commit

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -9,12 +9,12 @@ public class Duck5 {
     private Egg6 egg;
 
     @Autowired
-    public Duck5(Egg6 egg) {
+    public void setEgg(Egg6 egg) {
         this.egg = egg;
     }
-
     @Override
     public String toString() {
         return ", в утке яйцо " + egg.toString();
     }
+
 }
